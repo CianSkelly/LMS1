2f9f4545bbb0ccebe65a67063aaccf71a241af36
Cian <cianskelly@hotmail.com>
2018-05-10 20:42:15 +0100

    Added a book model

---
+++ LibraryManagementSystem/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryManagementSystem.Models
+{
+    public class Book
+    {
+        public int ISBN { get; set; }
+        public string Title { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
