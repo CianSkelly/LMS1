2b670b64c685b84b90cbbf52a964cd33c700c347
Philip Smith <philip.smith10@gmail.com>
2018-05-10 20:36:42 +0100

    added user Model

---
+++ LibraryManagementSystem/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryManagementSystem.Models
+{
+    public class User
+    {
+            public int user_Id { get; set; }
+            public string UserName { get; set; }
+            public string email_address { get; set; }
+            public string phone_number { get; set; }
+            //public DateTime EnrollmentDate { get; set; }
+
+            public virtual ICollection<Book_on_Loan> Books_on_Loan { get; set; }
+        }
+    }
+}
