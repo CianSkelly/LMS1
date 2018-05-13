e08ddafec6c2f4de0f4cf2e047f6aad937c015d4
Philip Smith <philip.smith10@gmail.com>
2018-05-13 15:42:08 +0100

    Fixed Book Model - Deleted and re-added

---
+++ LibraryManagementSystem/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryManagementSystem.Models
+{
+    public class Book
+    {
+        public int Book_Id { get; set; }
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public DateTime PublishDate { get; set; }
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
